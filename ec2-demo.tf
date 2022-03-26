2df0dbd51dd691f65b8b2172fe63b92e0cc6304f
gitkadam <102312568+gitkadam@users.noreply.github.com>
2022-03-26 11:25:26 +0530

    Create ec2-demo.tf

---
+++ ec2-demo.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "web" {
+  ami  = "ami-04893cdb768d0f9ee"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-02e70ebeb32953095"
+  key_name      = "2marchkey"
+  tags = {
+    Name = "Terraform_27Demo"
+    quality = "cerated by user"
+  }
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
+  user_data              = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd.service
+              systemctl enable httpd.service
+              echo "Welcome to Terraform Demo!!!, I am $(hostname -f) hosted by Terraform" > /var/www/html/index.html
+              EOF
+}
